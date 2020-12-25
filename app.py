d4ffdc0cceab9480f44d3a608e97ca11c7b82e7f
euguroglu <e.uguroglu@yahoo.com>
2020-12-25 17:51:01 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import render_template, Flask
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "Mysecret"
+
+class LoginForm(FlaskForm):
+    username = StringField('username')
+    password = PasswordField('password')
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    form = LoginForm()
+
+    return render_template('index.html',form=form)
+
+if __name__ == "__main__":
+    app.run(debug=True)
