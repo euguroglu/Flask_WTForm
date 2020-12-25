ce138a483c8af53a745a669c913ffef512b503e6
euguroglu <e.uguroglu@yahoo.com>
2020-12-25 22:12:18 +0300

    validators added

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import render_template, Flask
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, AnyOf
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "Mysecret"
 
 class LoginForm(FlaskForm):
-    username = StringField('username')
-    password = PasswordField('password')
+    username = StringField('username',validators=[InputRequired(),Length(min=4,max=8,message='Must be between 4 and 8 characters')])
+    password = PasswordField('password',validators=[InputRequired()])
 
 @app.route('/',methods=['GET','POST'])
 def index():
