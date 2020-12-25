20336aa27a7d929437d99be0b6db5bf6966ae95a
euguroglu <e.uguroglu@yahoo.com>
2020-12-25 22:26:39 +0300

    ıntegerfiled and boolean filed added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import render_template, Flask
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, BooleanField, IntegerField
 from wtforms.validators import InputRequired, Length, AnyOf
 
 app = Flask(__name__)
@@ -9,13 +9,15 @@ app.config['SECRET_KEY'] = "Mysecret"
 class LoginForm(FlaskForm):
     username = StringField('username',validators=[InputRequired(),Length(min=4,max=8,message='Must be between 4 and 8 characters')])
     password = PasswordField('password',validators=[InputRequired(),AnyOf(values=['secret','password'])])
+    age = IntegerField('age')
+    true = BooleanField('true')
 
 @app.route('/',methods=['GET','POST'])
 def index():
     form = LoginForm()
 
     if form.validate_on_submit():
-        return '<h1>Username: {} Password: {}</h1>'.format(form.username.data,form.password.data)
+        return '<h1>Username: {} Password: {} Age: {} True: {}</h1>'.format(form.username.data,form.password.data,form.age.data,form.true.data)
 
     return render_template('index.html',form=form)
 
